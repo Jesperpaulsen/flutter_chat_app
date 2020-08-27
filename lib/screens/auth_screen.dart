9e599e9addf84425a84415c696430a91d7115ef7
Jesper Paulsen <jesper@jesper.no>
2020-08-27 07:50:30 +0200

    Added firebase integration. Added auth and chat screen.

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_complete_guide/widgets/auth_form.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  var _isLoading = false;
+
+  void _submitAuthForm(String email, String password, String username,
+      bool isLogin, BuildContext context) async {
+    AuthResult authResult;
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        await Firestore.instance
+            .collection('users')
+            .document(authResult.user.uid)
+            .setData({
+          'username': username,
+          'email': email,
+        });
+      }
+    } on PlatformException catch (e) {
+      var message = 'An error occured, please check your credentials';
+
+      if (e.message != null) {
+        message = e.message;
+      }
+
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+    } catch (e) {
+      print(e);
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AuthForm(_submitAuthForm, _isLoading),
+    );
+  }
+}
